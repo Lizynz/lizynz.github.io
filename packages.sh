537619947bd7f4553de5cc3d416d7822f086b151
Lizynz <Lizynz@yandex.ru>
2018-05-12 14:45:30 +0700

    add vkShortcuts

--- packages.sh
+++ packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/FullFolder11 debs
+dpkg-deb -bZgzip projects/vkShortcuts debs
 echo .DS_Store >> .gitignore
 git add .gitignore
 git commit -m '.DS_Store banished!'
